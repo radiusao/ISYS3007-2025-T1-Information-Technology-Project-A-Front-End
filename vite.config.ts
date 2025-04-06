29cca6589b3186c20c3bf0cc5e12fde4de094f25
radiusao <puturaditya717@gmail.com>
2025-04-06 23:14:49 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ISYS3007-2025-T1-Information-Technology-Project-A-Front-End",
+  // base: "/",
   
 })
